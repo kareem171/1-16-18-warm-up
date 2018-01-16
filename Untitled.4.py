1bb618002f4e07d7f4d2a84a2b6b528aa6d77938
kareem171 <34690989+kareem171@users.noreply.github.com>
2018-01-16 13:38:32 -0500

    Add files via upload

---
+++ Untitled.4.py
@@ -0,0 +1,10 @@
+# print even or odd in a range of numbers
+
+
+for number in range (50):
+    if number%2==0:
+        print("Even number")
+    elif number %2==1:
+        print("Odd")
+    print(number)
+      
