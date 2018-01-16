de4e17e7c42b9ff2b5c4201eafd7a6c6ac132feb
kareem171 <34690989+kareem171@users.noreply.github.com>
2018-01-16 13:25:03 -0500

    Add files via upload

---
+++ Untitled.3.py
@@ -0,0 +1,18 @@
+from math import sqrt
+
+# using input() to make an interactive program
+x= float(input("Enter the number you want to sqaure root: "))
+numb_squared= sqrt(x)
+# sqrt(2.0)
+print(numb_squared)
+
+
+x= sqrt(2.0)
+print(x)
+y=2.0
+
+if x*x==y:
+      print(x)
+else:
+      print(y)
+      
